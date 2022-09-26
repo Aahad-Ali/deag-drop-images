a873fd14f20f9fa86394b46f7c40382a309fd5e2
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-09-26 19:52:19 +0500

    Add files via upload

---
+++ dreag & drop/app.js
@@ -0,0 +1,64 @@
+window.document.addEventListener('DOMContentLoaded', () => {
+
+    let items = document.querySelectorAll('.container .box');
+    let dragSrcEl = null;
+
+    function handleDragStart(e) {
+        dragSrcEl = this;
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+
+   
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+
+            e.preventDefault();
+        }
+
+        e.dataTransfer.dropEffect='move';
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        this.classList.add('over');
+    }
+
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+
+        if (e.stopPropagation) {
+
+            e.stopPropagation()
+        }
+
+        if (dragSrcEl != this) {
+
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        items.forEach(function (item) {
+            item.classList.remove('over');
+        });
+    }
+
+    items.forEach((item) => {
+        // console.log(item)
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+        console.log('item', item)
+    })
+
+});
